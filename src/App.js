589f12ea66b92b99c800ee197fe7c32dc81332ef
chohyunjungai <128457873+chohyunjungai@users.noreply.github.com>
2023-05-04 12:13:21 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,97 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import {
+  Button,
+  InputContainer,
+  PageWrapper,
+  TodoCard,
+  TodoContainer,
+  TodoHeader,
+  TodoListContainer,
+} from './components/styles';
+import nextId from 'react-id-generator';
+import { useDispatch, useSelector } from 'react-redux';
+import { __addToDo, __deleteTodo } from './redux/modules/todosSlice';
+import { waitTwoSeconds } from './utils.js';
 
 function App() {
+  const dispatch = useDispatch();
+  const todos = useSelector((state) => state.todos.list);
+  const [title, setTitle] = useState('');
+  const [body, setBody] = useState('');
+
+  const onAddTodo = async() => {
+    const newTodo = {
+      id: nextId(), 
+      title: title,
+      body: body,
+    };
+    await dispatch(__addToDo(newTodo));
+
+    // // 2초간 기다립니다.
+    // await waitTwoSeconds();
+  
+    // // 새로운 todo를 추가하기 위해 dispatch를 호출합니다.
+    // dispatch(__addToDo(newTodo));
+  
+    // 입력값을 초기화합니다.
+    resetInputs();
+    /**
+     * 시험 문제 1.
+     * 이곳에서 추가하기 기능을 구현해주세요.
+     */
+  };
+
+  const onDeleteTodo = async (id) => {
+    await waitTwoSeconds();
+    dispatch(__deleteTodo({
+      id: id,
+    }));
+    /**
+     * 시험 문제 2.
+     * 이곳에서 삭제하기 기능을 구현해주세요.
+     */
+  };
+
+  const resetInputs = () => {
+    /**
+     * 입력 값을 초기화하고 싶다면 사용하세요.
+     */
+    setTitle('');
+    setBody('');
+  };
+  const onChangeTitle = (e) => setTitle(e.target.value);
+  const onChangeBody = (e) => setBody(e.target.value);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <PageWrapper>
+      <TodoContainer>
+        <TodoHeader>🐢 SLOW TODO LIST 🐢</TodoHeader>
+        <InputContainer>
+          <span>제목: </span>
+          <input
+            value={title}
+            placeholder="할 일 제목"
+            onChange={onChangeTitle}
+          />
+          <span>내용: </span>
+          <input
+            value={body}
+            placeholder="할 일 내용"
+            onChange={onChangeBody}
+          />
+
+          <Button onClick={onAddTodo}>+ 추가하기</Button>
+        </InputContainer>
+        <TodoListContainer>
+          {todos.map((todo) => (
+            <TodoCard key={todo.id}>
+              <span>제목: {todo.title}</span>
+              <span>할 일: {todo.body}</span>
+            <Button onClick={()=>onDeleteTodo(todo.id)}>삭제하기</Button>
+            </TodoCard>
+          ))}
+        </TodoListContainer>
+      </TodoContainer>s
+    </PageWrapper>
   );
 }
 
